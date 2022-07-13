bd382b95e65220cd4a13f23d0396586e0c2ba0d5
Karol Knapik <knapikkarol3@gmail.com>
2022-07-13 16:07:19 +0200

    Update of  project

--- myScript_eshop.js
+++ myScript_eshop.js
@@ -1,66 +1,65 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
- /*-- Pridanie dat z JSON do Top produkty,slider --*/
-   
-   $.get("db.json",function(produkty){ 
-   for( i= 0; i<4; i++){
-   
-   $(`#slid${i+1} h2`).html(produkty[i].nazov);
-   $(`#slid${i+1} h3`).html(produkty[i].nazov1);
-   $(`#img${i+1}`).attr("src", produkty[i].img);
-   }
-   });
+  /*-- Pridanie dat z JSON do Top produkty,slider --*/
 
-   /*-- Tlacidlo na pridanie do kosika --*/
-   let produkt1 = JSON.parse(localStorage.getItem("produkty"));
-   $("button.btn4").on('click',function(event) { 
-    let  produkt = JSON.parse(localStorage.getItem("produkty"));
-      if(produkt == null){
-            produkt = [];
-         }
+  $.get("db.json", function (produkty) {
+    for (i = 0; i < 4; i++) {
 
-    let  klikloSaNa = $(event.target).attr('idproduktu');
+      $(`#slid${i + 1} h2`).html(produkty[i].nazov);
+      $(`#slid${i + 1} h3`).html(produkty[i].nazov1);
+      $(`#img${i + 1}`).attr("src", produkty[i].img);
+    }
+  });
 
-      $.get("db.json",function(produktyDB){
-         for(let produkt of produktyDB ){
-                if(klikloSaNa == produkt.id){
-                    produkt1.push(produkt);
-                    localStorage.setItem("produkty", JSON.stringify(produkt1))
-                    return;
-                }
-            }
-         });
-         function pocitadlo() {
-            let kosik = JSON.parse(localStorage.getItem("produkty"));
-            $(`#pocitadlo`).html(kosik.length+1);
-            console.log(kosik.length);
-          }
-    
-          pocitadlo();
-       
-      });
+  /*-- Tlacidlo na pridanie do kosika --*/
+  let produkt1 = JSON.parse(localStorage.getItem("produkty"));
+  $("button.btn4").on('click', function (event) {
+    let produkt = JSON.parse(localStorage.getItem("produkty"));
+    if (produkt == null) {
+      produkt = [];
+    }
 
-    /*-- Prechod na detail produktu --*/
-      $("button.btn3").on('click',function(event) { 
-      klikloSaNa1 = $(event.target).attr('idproduktu');
-      window.location.replace("./detail.html?id="+ klikloSaNa1);
-               
-      });
+    let klikloSaNa = $(event.target).attr('idproduktu');
 
-    /*-- Odoslanie dat z kontaktneho formulara --*/
-    $('#odoslat').on('click',function() {
-      var postData = $("#myForm1").serialize()
-      alert("Tieto data posielam na server" + " " + postData );
- 
-      });
-      function pocitadlo() {
-         let kosik = JSON.parse(localStorage.getItem("produkty"));
-         $(`#pocitadlo`).html(kosik.length);
-         console.log(kosik.length);
-       }
- 
-       pocitadlo();
-    
+    $.get("db.json", function (produktyDB) {
+      for (let produkt of produktyDB) {
+        if (klikloSaNa == produkt.id) {
+          produkt1.push(produkt);
+          localStorage.setItem("produkty", JSON.stringify(produkt1))
+          return;
+        }
+      }
+    });
+    function pocitadlo() {
+      let kosik = JSON.parse(localStorage.getItem("produkty"));
+      $(`#pocitadlo`).html(kosik.length + 1);
+    }
+    pocitadlo();
 
+  });
+
+  /*-- Prechod na detail produktu --*/
+  $("button.btn3").on('click', function (event) {
+    klikloSaNa1 = $(event.target).attr('idproduktu');
+    window.location.replace("./detail.html?id=" + klikloSaNa1);
+
+  });
+
+  /*-- Odoslanie dat z kontaktneho formulara --*/
+  $('#odoslat').on('click', function () {
+    var postData = $("#myForm1").serialize()
+    alert("Tieto data posielam na server" + " " + postData);
+
+  });
+  function pocitadlo() {
+    let kosik = [];
+    if (JSON.parse(localStorage.getItem("produkty")) === null) {
+      // kosik.length = 0
+      $(`#pocitadlo`).html(kosik);
+    }
+    $(`#pocitadlo`).html(kosik.length);
+    // console.log(kosik.length);
+  }
+  pocitadlo();
 });
--- myScript_kosik.js
+++ myScript_kosik.js
@@ -1,89 +1,96 @@
-let produkty;
 
-$(document).ready(function(){
+
+$(document).ready(function () {
 
   /*-- Pridanie dát do tabulky kosika --*/
-  produkty=JSON.parse(localStorage.getItem("produkty"));
-   
-  for(let i= 0; i<produkty.length; i++){
-  $('#tabulka1').append(`<tr><td>${produkty[i].id }</td><td>${produkty[i].nazov}</td><td><input id="pocet${i}" style="width: 15%;" type="number" min=1 class=number name="pocet" value="${produkty[i].pocet}"></td><td id="aktcena${i}">${produkty[i].cena * produkty[i].pocet } </td><td><input id="checkBox${i}" class=checkBox type="checkbox"></td></tr>`);
+  let produkty = [];
+  if (JSON.parse(localStorage.getItem("produkty")) == null) {
+    produkty.length = 0
   }
-  
+  else {
+    produkty = JSON.parse(localStorage.getItem("produkty"))
+  }
+
+
+  for (let i = 0; i < produkty.length; i++) {
+    $('#tabulka1').append(`<tr><td>${produkty[i].id}</td><td>${produkty[i].nazov}</td><td><input id="pocet${i}" style="width: 15%;" type="number" min=1 class=number name="pocet" value="${produkty[i].pocet}"></td><td id="aktcena${i}">${produkty[i].cena * produkty[i].pocet} </td><td><input id="checkBox${i}" class=checkBox type="checkbox"></td></tr>`);
+  }
+
   $(":input").on('keyup mouseup', function () {
-    let ceny=[];
-    for(let i= 0; i<produkty.length; i++){
-  
-    this.cena = produkty[i].cena
-    let cena1 = parseInt(this.cena);
-    let aktcena = cena1 * parseInt($(`#pocet${i}`).val());
-    aktcena = parseInt(aktcena)
-    console.log(aktcena);
-    document.getElementById(`aktcena${i}`).innerHTML = aktcena
-    ceny.push(aktcena);
-    console.log(ceny);
+    let ceny = [];
+    for (let i = 0; i < produkty.length; i++) {
 
-    produkty[i].pocet = parseInt($(`#pocet${i}`).val());
+      this.cena = produkty[i].cena
+      let cena1 = parseInt(this.cena);
+      let aktcena = cena1 * parseInt($(`#pocet${i}`).val());
+      aktcena = parseInt(aktcena)
+      console.log(aktcena);
+      document.getElementById(`aktcena${i}`).innerHTML = aktcena
+      ceny.push(aktcena);
+      console.log(ceny);
+
+      produkty[i].pocet = parseInt($(`#pocet${i}`).val());
     }
-    localStorage.setItem("produkty",JSON.stringify(produkty));
+    localStorage.setItem("produkty", JSON.stringify(produkty));
 
-  
-   /*-- Vypocet ceny veci v kosiku: Bez dph/Spolu --*/
-  if(produkty!=0){
-    let reducer = (accumulator, currentValue) => accumulator + currentValue ;
-    let spolu = ceny.reduce(reducer)
-    console.log(spolu)
-  $("#spolu").val(spolu+" " + "EUR");
 
-    let spoluBezDph = spolu -( spolu * 0.19);
-    console.log(spoluBezDph);
-  $("#spoluBezDph").val(spoluBezDph+" " + "EUR");
-  }
+    /*-- Vypocet ceny veci v kosiku: Bez dph/Spolu --*/
+    if (produkty != 0) {
+      let reducer = (accumulator, currentValue) => accumulator + currentValue;
+      let spolu = ceny.reduce(reducer)
+      console.log(spolu)
+      $("#spolu").val(spolu + " " + "EUR");
 
-})
+      let spoluBezDph = spolu - (spolu * 0.19);
+      console.log(spoluBezDph);
+      $("#spoluBezDph").val(spoluBezDph + " " + "EUR");
+    }
+
+  })
 
 
   /*-- Vypocet ceny veci v kosiku: Bez dph/Spolu --*/
   let ceny = [];
-  for(let i= 0; i<produkty.length; i++){
+  for (let i = 0; i < produkty.length; i++) {
     let cena = produkty[i].cena
-    cena1=parseInt(cena)
+    cena1 = parseInt(cena)
     aktcena = cena1 * parseInt($(`#pocet${i}`).val());
     aktcena = parseInt(aktcena)
     ceny.push(aktcena)
   }
 
-  if(produkty!=0){
+  if (produkty != 0) {
     let reducer = (accumulator, currentValue) => accumulator + currentValue;
     let spolu = ceny.reduce(reducer)
-  $("#spolu").val(spolu+ " " +"EUR");
+    $("#spolu").val(spolu + " " + "EUR");
 
-    let spoluBezDph = spolu -( spolu * 0.19);
-  $("#spoluBezDph").val(spoluBezDph+" " + "EUR");
+    let spoluBezDph = spolu - (spolu * 0.19);
+    $("#spoluBezDph").val(spoluBezDph + " " + "EUR");
   }
 
   /*-- Button : vyprazdnit kosik --*/
-  $('#button2').on('click',function deleteItem() {
-    for(let i=produkty.length; i>=0; i--){
-        produkty.splice(i,1);
-        
+  $('#button2').on('click', function deleteItem() {
+    for (let i = produkty.length; i >= 0; i--) {
+      produkty.splice(i, 1);
+
     }
-    localStorage.setItem("produkty",JSON.stringify(produkty));
+    localStorage.setItem("produkty", JSON.stringify(produkty));
     location.reload()
-  }) 
+  })
 
 
   /*-- Button : zmazat zvolenu polozku v kosiku --*/
-  $('#button3').on('click',function deleteItem() {
-    for(let i=produkty.length; i>=0; i--){
+  $('#button3').on('click', function deleteItem() {
+    for (let i = produkty.length; i >= 0; i--) {
       let status = $(`#checkBox${i}:checked`).val();
-      if(status){
-        produkty.splice(i,1);
-        
+      if (status) {
+        produkty.splice(i, 1);
+
+      }
     }
-    }
-    localStorage.setItem("produkty",JSON.stringify(produkty));
+    localStorage.setItem("produkty", JSON.stringify(produkty));
     location.reload();
-    
+
   })
 
 
